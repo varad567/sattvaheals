52b57804b703d52b568c09f97af2e1f9398c0f65
varad567 <72659694+varad567@users.noreply.github.com>
2026-03-07 05:24:43 +0000

    Pending changes done for commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SattvaLanding from './SattvaLanding'
+import SattvaAuth from './SattvaAuth'
 
 export default function App() {
-  return <SattvaLanding />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SattvaLanding />} />
+        <Route path="/signup" element={<SattvaAuth defaultTab="signup" />} />
+        <Route path="/login" element={<SattvaAuth defaultTab="login" />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
