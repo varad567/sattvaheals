d46304923f0e76428304929a200e84676858cf4a
varad567 <72659694+varad567@users.noreply.github.com>
2026-03-07 13:55:45 +0530

    feat - pushing About

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SattvaLanding from './SattvaLanding'
 import SattvaAuth from './SattvaAuth'
 import HowItWorks from './HowItWorks'
+import About from './About'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/signup" element={<SattvaAuth defaultTab="signup" />} />
         <Route path="/login" element={<SattvaAuth defaultTab="login" />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
