bd2678abdf46b7dd9f3f8703a256946b00ab1afb
varad567 <72659694+varad567@users.noreply.github.com>
2026-03-14 18:48:40 +0000

    Resolv  1

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Dashboard from './Dashboard'
 import Issues from './Issues'
 import IssueDetail from './IssueDetail'
 import Terms from './Terms'
-import BirthChart from './BirthChart'
+import BirthChart from './Birthchart'
 
 export default function App() {
   const [user, setUser] = useState(null)
@@ -54,7 +54,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/terms" element={<Terms />} />
-        <Route path="/birth-chart" element={<BirthChart />} />
+        <Route path="/birthchart" element={<BirthChart />} />
       </Routes>
     </BrowserRouter>
   )
